8efea217cbfa92805404cba021505cd36617c107
Guillermo Santander <guillermosantander343@gmail.com>
2024-06-22 10:23:52 -0400

    Implementacion_Clientes

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import random
 
-clientes = []
-Saldo = [0]
+clientes = [
+    "Cliente Maria Carmen",
+    "Cliente Eduardo Perez",
+    "Cliente Felipe Gonzales",
+    "Cliente Diego Causa",
+    "Cliente Kevin levin",
+    "Cliente Daniel Mandarina",
+    "Cliente Rocio Andrades",
+    "Cliente Abraham Explocivo",
+    "Cliente Simba Scar",
+    "Cliente Jose Antonio",
+    ]
