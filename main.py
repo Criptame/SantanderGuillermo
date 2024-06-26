bab5f85a2c1776ed5ccbf51923a86a1c9dbfc618
Guillermo Santander <guillermosantander343@gmail.com>
2024-06-25 20:34:01 -0400

    Nombres

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import csv
 
+import random
+saldo = [0]
+
 clientes = [
-    "Maria Carmen",
-    "Eduardo Perez",
-    "Felipe Gonzales",
-    "Diego Causa",
-    "Kevin levin",
-    "Daniel Mandarina",
-    "Rocio Andrades",
-    "Abraham Explocivo",
-    "Simba Scar",
-    "Jose Antonio",
+    ['Nombre'],
+    ['Juan Aguuirres'],
+    ['Ana Maria'],
+    ['Luis Ector'],
+    ['Diego Causa'],
+    ['Kevin levin'],
+    ['Daniel Mandarina'],
+    ['Rocio Andrades'],
+    ['Abraham Explocivo'],
+    ['Simba Scar'],
+    ['Jose Castro'],
     ]
+
+
 
 with open('datos.csv', mode='w', newline='') as file:
     writer = csv.writer(file)
@@ -22,4 +28,4 @@ print("Archivo CSV 'datos.csv' guardado correctamente")
 with open('datos.csv', mode='r', newline='') as file:
     reader = csv.reader(file)
     row = list(reader)
-
+    
