485a56ca7d65b1bf8b8955cee24077d3a4f78b56
Guillermo Santander <guillermosantander343@gmail.com>
2024-06-27 19:55:04 -0400

    Saldos

--- main.py
+++ main.py
@@ -1,64 +1,35 @@
+
 import csv
 
 import random
-saldo = [0]
 
 clientes = [
     ['Nombre',  'Dinero'],
-    ['Juan Aguuirres', '10000'],
-    ['Ana Maria',  '10000'],
-    ['Luis Ector',  '5000'],
-    ['Diego Causa',  '9000'],
-    ['Kevin levin',  '20000'],
-    ['Daniel Mandarina',  '2000'],
-    ['Rocio Andrades',  '30000'],
-    ['Abraham Explocivo',  '5000'],
-    ['Simba Scar',  '10000'],
-    ['Jose Castro',  '30000'],
+    ['Juan Aguuirres', ],
+    ['Ana Maria', ],
+    ['Luis Ector',],
+    ['Diego Causa', ],
+    ['Kevin levin', ],
+    ['Daniel Mandarina',],
+    ['Rocio Andrades', ],
+    ['Abraham Explocivo', ],
+    ['Simba Scar',],
+    ['Jose Castro',],
     ]
 
 print("Elija una opcion")
 print("Opcion 1- Ver saldo De Clientes")
-print("Opcion 2- Elejir cliente")
+print("Opcion 2- Ver a un cliente cliente")
 print("Opcion 3- Agregar saldo al clientes")
 print("Opcion 4- Modicar lista Vase a saldo")
 print("Opcion 5- Salir del programa")
 
-while True:
-    opcion = int(input())
-    if opcion == 1:
-        print(clientes)
-    elif opcion == 2:
-        cliente = random.choice(clientes)
-        print(cliente)
-    elif opcion == 3:
-            print("Ingrese al cliente")
-            selecion_cliente = int(input())
-            print("Ingrese el monto a agregar")
-            monto = int(input())
-            saldo.append(monto)
-            print("Haora {selecion_cliente} tiene {monto} en su cuenta")
-    elif opcion == 4:
-        print("Ingrese el indice del cliente a modificar")
-        indice = int(input())
-        print("Ingrese el nuevo saldo")
-        monto = int(input())
-        saldo[indice] = monto
-        print("Saldo modificado correctamente")
-    elif opcion == 5:
-        print("Gracias por usar nuestro programa")
-        break
-    else:
-        print("Opcion no valida")
-        print("Elija una opcion")
-        print("Opcion 1- Ver saldo De Clientes")
-        print("Opcion 2- Elejir cliente")
-        print("Opcion 3- Agregar saldo al clientes")
-        print("Opcion 4- Modicar lista Vase a saldo")
-        print("Opcion 5- Salir del programa")
-        continue
-    break
+def generardor_de_saldos_Aleatorios():
+    return [random.randint(100, 10000000)]
 
+def Definir_Saldo(saldos):
+    Rango = {"Menor": (1000, 30000), "Regular": (35000, 100000), "Mayor": (150000, 10000000)}
+    clasificar = {"Menor": [], "Regular": [], "Mayor": []}
 
 with open('datos.csv', mode='w', newline='') as file:
     writer = csv.writer(file)
