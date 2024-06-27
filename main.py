10676996875996962eeb7944a83216fd07135f9b
Guillermo Santander <guillermosantander343@gmail.com>
2024-06-27 00:23:08 -0400

    Opciones

--- main.py
+++ main.py
@@ -17,6 +17,29 @@ clientes = [
     ['Jose Castro'],
     ]
 
+print("Elija una opcion")
+print("Opcion 1- Ver saldo De Clientes")
+print("Opcion 2- Elejir cliente")
+print("Opcion 3- Agregar saldo al clientes")
+
+while True:
+    opcion = int(input())
+    if opcion == 1:
+        print(clientes)
+    elif opcion == 2:
+        cliente = random.choice(clientes)
+        print(cliente)
+    elif opcion == 3:
+            print("Ingrese el monto a agregar")
+            monto = int(input())
+            saldo.append(monto)
+    else:
+        print("Opcion no valida")
+        print("Elija una opcion")
+        print("Opcion 1- Ver saldo De Clientes")
+        print("Opcion 2- Elejir cliente")
+        continue
+    break
 
 
 with open('datos.csv', mode='w', newline='') as file:
